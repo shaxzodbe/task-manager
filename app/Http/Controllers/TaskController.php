2f38ec8756b22692a48969d7562b25e1f8a21cc3
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2024-03-03 00:15:43 +0500

    task controller index changed

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskCollection;
 use App\Models\Task;
 
 class TaskController extends Controller
@@ -13,7 +14,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        return new TaskCollection(Task::all());
     }
 
     /**
