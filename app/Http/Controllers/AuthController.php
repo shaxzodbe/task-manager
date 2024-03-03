e9d3e5be281a589faa845acf44d0c3eafe9479f3
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2024-03-03 21:43:20 +0500

    auth controller login & register methods changed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(Request $request): JsonResponse
     {
         $validated = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required'
+          'email' => 'required|email',
+          'password' => 'required'
         ]);
 
         if (!Auth::attempt($validated)) {
@@ -22,8 +23,25 @@ class AuthController extends Controller
         $user = User::where('email', $validated['email'])->first();
 
         return response()->json([
-            'access_token' => $user->createToken('api_token')->plainTextToken,
-            'token_type' => 'Bearer'
+          'access_token' => $user->createToken('api_token')->plainTextToken,
+          'token_type' => 'Bearer'
         ]);
+    }
+
+    public function register(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+          'name' => 'required|max:255',
+          'email' => 'required|max:255|email|unique:users',
+          'password' => 'required|confirmed|min:6',
+        ]);
+
+        $user = User::create($validated);
+
+        return response()->json([
+          'data' => $user,
+          'access_token' => $user->createToken('api_token')->plainTextToken,
+          'token_type' => 'Bearer'
+        ], 201);
     }
 }
