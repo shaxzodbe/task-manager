31eab7e0c3e332e49b3aff84941eedbb0920c860
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2024-03-04 15:34:34 +0500

    task update request changed

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class UpdateTaskRequest extends FormRequest
 {
@@ -22,8 +24,14 @@ class UpdateTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'sometimes|required|max:255',
-            'is_done' => 'sometimes|boolean'
+          'title' => 'sometimes|required|max:255',
+          'is_done' => 'sometimes|boolean',
+          'project_id' => [
+            'nullable',
+            Rule::exists('projects', 'id')->where(function ($query) {
+                $query->where('creator_id', Auth::id());
+            })
+          ]
         ];
     }
 }
