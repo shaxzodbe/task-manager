149069d8da431d7de77c04dead3d18b828704636
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2024-03-04 13:10:38 +0500

    add project id migration changed

--- database/migrations/2024_03_03_170632_add_project_id_to_tasks_table.php
+++ database/migrations/2024_03_03_170632_add_project_id_to_tasks_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+        Schema::table('tasks', function (Blueprint $table) {
             $table->foreignId('project_id')->nullable()->constrained();
-            $table->timestamps();
         });
     }
 
