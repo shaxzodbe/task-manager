6a541d452084777fbae1dc516c448d38c42add60
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2024-03-04 12:49:56 +0500

    project resource index & store methods changed

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,24 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
+use App\Http\Resources\ProjectResource;
 use App\Models\Project;
+use Illuminate\Support\Facades\Auth;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ProjectController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): ProjectResource
     {
-        //
+        $projects = QueryBuilder::for(Project::class)
+          ->allowedFilters('title')
+          ->defaultSort('-created_at')
+          ->allowedSorts(['title', 'created_at'])
+          ->paginate();
+
+        return new ProjectResource($projects);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreProjectRequest $request)
+    public function store(StoreProjectRequest $request): ProjectResource
     {
-        //
+        $validated = $request->validated();
+
+        $project = Auth::user()->projects()->create($validated);
+
+        return new ProjectResource($project);
     }
 
     /**
