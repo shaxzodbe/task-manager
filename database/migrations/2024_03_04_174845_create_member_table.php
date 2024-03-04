f171686616a18e0287b63f0e6680c0d8775c09e4
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2024-03-04 22:53:38 +0500

    member pivot model, migration changed

--- database/migrations/2024_03_04_174845_create_member_table.php
+++ database/migrations/2024_03_04_174845_create_member_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('member', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
         });
     }
 
